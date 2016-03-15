e8673fdddb6b122046036d559cdf3f80f80ee26e
Chris Fallin <cfallin@c1f.net>
2016-03-15 15:19:36 -0400

    oping 0.2.0 docs.

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['oping'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='oping/enum.AddrFamily.html' title='oping::AddrFamily'>AddrFamily</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='oping/struct.PingItem.html' title='oping::PingItem'>PingItem</a>",];implementors['oping'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='oping/enum.AddrFamily.html' title='oping::AddrFamily'>AddrFamily</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='oping/struct.PingItem.html' title='oping::PingItem'>PingItem</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
