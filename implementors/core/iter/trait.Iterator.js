39a0e4b1ffc40d7d6614e4f300251c931038a982
Chris Fallin <cfallin@c1f.net>
2016-03-15 15:29:23 -0400

    Updated docs.

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['oping'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='oping/struct.PingIter.html' title='oping::PingIter'>PingIter</a>",];implementors['oping'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='oping/struct.PingIter.html' title='oping::PingIter'>PingIter</a>",];
+implementors['libc'] = [];implementors['oping'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='oping/struct.PingIter.html' title='oping::PingIter'>PingIter</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
